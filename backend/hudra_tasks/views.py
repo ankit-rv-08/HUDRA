454fd97d8cf260e18b3a0469f73646ec408e6493
ankit-rv-08 <ankith8804@gmail.com>
2026-03-16 10:38:14 +0000

    updates

--- backend/hudra_tasks/views.py
+++ backend/hudra_tasks/views.py
@@ -6,7 +6,9 @@ from .models import Task
 from .serializers import TaskSerializer
 
 
-class TaskViewSet(viewsets.ViewSet):
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
 
     permission_classes = [IsAuthenticated]
 
