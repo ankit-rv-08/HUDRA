2323c920b23939a337582834f79f7db0aa667a67
ankit-rv-08 <ankith8804@gmail.com>
2026-03-16 10:24:10 +0000

    HUDRA task model updated

--- backend/hudra_tasks/serializers.py
+++ backend/hudra_tasks/serializers.py
@@ -1,9 +1,8 @@
-# serializers.py
 from rest_framework import serializers
 from .models import Task
 
 class TaskSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Task
-        fields = ['id', 'title', 'description', 'reward', 'is_completed', 'is_paid', 'created_at', 'created_by']
-        read_only_fields = ['created_at', 'created_by', 'is_completed', 'is_paid']
+        fields = "__all__"
