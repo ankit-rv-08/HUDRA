4db402ff375330a460170e12a3ddb047fdc87ab6
ankit-rv-08 <ankith8804@gmail.com>
2026-03-16 10:50:14 +0000

    HUDRA

--- backend/hudra_tasks/models.py
+++ backend/hudra_tasks/models.py
@@ -1,12 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Task(models.Model):
-
     title = models.CharField(max_length=200)
     location = models.CharField(max_length=200)
     budget = models.DecimalField(max_digits=8, decimal_places=2)
 
-    description = models.TextField(blank=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
+    is_completed = models.BooleanField(default=False)
+    is_paid = models.BooleanField(default=False)
 
     created_at = models.DateTimeField(auto_now_add=True)
 
