a5af01575fbbd35fc5fb6d36e0c7eb0b35109c7e
ankit-rv-08 <ankith8804@gmail.com>
2026-03-16 11:27:03 +0000

    update

--- backend/hudra/settings.py
+++ backend/hudra/settings.py
@@ -110,8 +110,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # REST_FRAMEWORK + JWT
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny',
     ),
 }
 
