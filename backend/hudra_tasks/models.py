37ab0ce8732b7ce761225e4cfc9c5b373d9acd8d
ankit-rv-08 <ankith8804@gmail.com>
2026-03-16 17:37:48 +0000

    fix serializers

--- backend/hudra_tasks/models.py
+++ backend/hudra_tasks/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Task(models.Model):
     title = models.CharField(max_length=200)
     location = models.CharField(max_length=200)
