053041cf9c39ffe4bb82ea617835f7d60e30afd8
ankit-rv-08 <ankith8804@gmail.com>
2026-03-16 18:03:53 +0000

    1

--- backend/hudra_tasks/views.py
+++ backend/hudra_tasks/views.py
@@ -21,10 +21,8 @@ class TaskViewSet(viewsets.ModelViewSet):
         serializer = self.get_serializer(data=request.data) 
         serializer.is_valid(raise_exception=True)       
         
-        if request.user.is_authenticated:   
-            serializer.save(created_by=request.user)
-        else:
-            serializer.save()
+        user = request.user if request.user.is_authenticated else None
+        serializer.save(created_by=user)
 
         return Response(serializer.data)
 
